acb459add65b14301cb2d0b407b39b09ca5a0dcb
Adham <adham.a.altony@gmail.com>
2026-01-11 20:50:48 +0200

    Configure root path for /tools mounting

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 
 from app.routes.tiktok import router as tiktok_router
@@ -5,15 +7,16 @@ from app.routes.youtube import router as youtube_router
 from app.routes.downloads import router as downloads_router
 from app.routes.pdf import router as pdf_router
 
-API_PREFIX = "/tools"
+ROOT_PATH = os.environ.get("ROOT_PATH", "")
 
 app = FastAPI(
-    openapi_url=f"{API_PREFIX}/openapi.json",
-    docs_url=f"{API_PREFIX}/docs",
-    redoc_url=f"{API_PREFIX}/redoc",
+    root_path=ROOT_PATH,
+    openapi_url="/openapi.json",
+    docs_url="/docs",
+    redoc_url="/redoc",
 )
 
-app.include_router(youtube_router, prefix=API_PREFIX)
-app.include_router(tiktok_router, prefix=API_PREFIX)
-app.include_router(downloads_router, prefix=API_PREFIX)
-app.include_router(pdf_router, prefix=API_PREFIX)
+app.include_router(youtube_router)
+app.include_router(tiktok_router)
+app.include_router(downloads_router)
+app.include_router(pdf_router)
